cf833cafeebc445400f87214c9af7c0e6564f71a
Souad Himel <souad.anam@gmail.com>
2022-02-17 18:32:52 +0600

    text

---
+++ script.js
@@ -0,0 +1,5 @@
+let totalIncome = document.getElementById("income");
+let foodExpenses = document.getElementById("foodExpenses");
+let rentExpenses = document.getElementById("rentExpenses");
+let clothesExpenses = document.getElementById("clothesExpenses");
+console.log(foodExpenses);
