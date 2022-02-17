51b63288517796311c48f75cfe647970f89e2a73
Souad Himel <souad.anam@gmail.com>
2022-02-17 20:48:18 +0600

    text

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let totalIncome = document.getElementById("income");
 let foodExpenses = document.getElementById("foodExpenses");
 let rentExpenses = document.getElementById("rentExpenses");
+let totalExpenses = document.getElementById("expensesOutput");
 let clothesExpenses = document.getElementById("clothesExpenses");
-console.log(foodExpenses);
+// console.log(foodExpenses);
+function calculation() {
+  const expensesTotal =
+    parseFloat(foodExpenses.value) +
+    parseFloat(rentExpenses.value) +
+    parseFloat(clothesExpenses.value);
+  totalExpenses.value = expensesTotal;
+  const balance = parseFloat(totalIncome.value) - expensesTotal;
+}
